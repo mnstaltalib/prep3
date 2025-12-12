cfa4f28b3b95f55ce23dfa0a580613ce2e13702a
mnstaltalib <mnst.altalib@gmail.com>
2025-12-12 03:20:57 +0200

    Add files via upload

--- data/prep3/science.js
+++ data/prep3/science.js
@@ -3,11 +3,12 @@ export const scienceData = {
     name: "العلوم",
     icon: "🧬", 
 
-    isLocked: true,        // هل المادة كلها مغلقة؟
-        isPdfLocked: true,     // هل المكتبة الرقمية مغلقة؟
+    isLocked: false,        // هل المادة كلها مغلقة؟
+        isPdfLocked: false,     // هل المكتبة الرقمية مغلقة؟
         isQuizLocked: true,     // هل الاختبارات مغلقة؟ 
     pdfs: [
-        { title: "اسم المذكرة", link: "#", size: "5MB" }
+        { title: "ملخص الأضواء", link: "https://drive.usercontent.google.com/download?id=1DrWoevh09QokMdW18kUosciY5YsOce0Q&export=download&authuser=1&confirm=t&uuid=8a4724cf-691f-4ff9-93ae-fe387d5bd97d&at=ALWLOp6o9unNDJnId3nzKUZRhwQP:1765502036519", size: "51MB" },
+        { title: "ملخص الامتحان", link: "https://drive.usercontent.google.com/download?id=1OCgMnc0PeP3xD8efF37ekTueEqI4NVYK&export=download&authuser=1&confirm=t&uuid=ad157fcd-c5c6-41a5-a6c7-981059d3726c&at=ALWLOp5VaVDAZZ4W_jNGKv3vKINS:1765502217989", size: "82MB" },
     ],
     lessons: [
 {
