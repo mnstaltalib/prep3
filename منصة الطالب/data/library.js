970edda17a44b55b460ed5e14e38fd2affe93637
mnstaltalib <mnst.altalib@gmail.com>
2026-04-10 15:55:54 +0200

    arabic

---
+++ منصة الطالب/data/library.js
@@ -0,0 +1,17 @@
+// data/library.js
+window.libraryData = [
+    // اللغة العربية
+    { title: "ملخص الاضواء", subject: "عربي", term: "الترم الثاني", link: "https://drive.google.com/file/d/1z_NPKvcIK7XXUBXgWnLQUygHEqyKB12a/view" },
+
+    // العلوم
+    { title: "ملخص الاضواء", subject: "علوم", term: "الترم الثاني", link: "https://drive.google.com/file/d/1ipgQmiIW6xL7ev0UASalIzcos64IWMbd/view" },
+
+    // الرياضيات
+    { title: "ملخص الاضواء", subject: "رياضيات", term: "الترم الثاني", link: "https://drive.google.com/file/d/1g9AsrKId36EickrMLCQa_vU2NXoxhZAC/view" },
+
+    // الدراسات
+    { title: "ملخص الاضواء", subject: "دراسات", term: "الترم الثاني", link: "https://drive.google.com/file/d/1oqacpCTnYvH_q9nmtW8iT0QFrk5AILdU/view" },
+
+    // الإنجليزي
+    { title: "ملخص GEM", subject: "انجليزي", term: "الترم الثاني", link: "https://drive.google.com/file/d/15fLyhjH2mso2vtzA1Z2zUPz8LZTAqPbm/view" },
+];
